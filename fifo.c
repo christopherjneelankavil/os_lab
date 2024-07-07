2c7ae5b8bf531b6a52dae7f426ba389d1bdf5a44
christopherneelankavil <christopherneelankavil@gmail.com>
2024-07-07 17:21:58 +0000

    updated fifo.c

--- fifo.c
+++ fifo.c
@@ -1,64 +1,67 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(){
-    
-    //reference string
+
+    //reference string 
     int n,i;
-    int pageHit=0,pageFault=0;
-    printf("Enter the no.of elements in reference string: ");
+    printf("Enter the total length : ");
     scanf("%d",&n);
     int ref[n];
-    printf("Enter the elements of the reference string : ");
+    printf("Enter the reference string : ");
     for(i=0;i<n;i++){
         scanf("%d",&ref[i]);
     }
-    
-    //frames 
-    int f,j;
-    printf("Enter the no.of frames: ");
-    scanf("%d",&f);
-    int fr[f];
-    for(int j=0;j<f;j++){
-        fr[j]=-1;
+
+    //frames
+    int fr,j;
+    printf("Enter the no.of frames : ");
+    scanf("%d",&fr);
+    int frames[fr];
+    for(j=0;j<fr;j++){
+        frames[j]= -1;
     }
-    
-    //fifo page replacement process 
-    printf("The page replacement process is : \n");
-    int count=0;
+
+    //page replacement
+    printf("The fifo page replacement process is : \n");
+    int count=0,pageHit=0,pageFault=0;
     for(i=0;i<n;i++){
+
         int found=0;
         
-        for(j=0;j<f;j++){
-            if(fr[j]==ref[i]){
+        for(j=0;j<fr;j++){
+            
+            if(frames[j]==ref[i]){
                 found=1;
                 pageHit++;
                 break;
             }
+
         }
         if(!found){
-            fr[count]=ref[i];
-            count=(count+1)%f;
             pageFault++;
+            frames[count]=ref[i];
+            count=(count+1)%fr;
         }
-        for(int k=0;k<f;k++){
-            if(fr[k]==-1){
+
+        for(int k=0;k<fr;k++){
+            if(frames[k]==-1){
                 printf("\t-");
             }
             else{
-                printf("\t%d",fr[k]);
+                printf("\t%d",frames[k]);
             }
         }
         if(found){
-            printf("\tPage hit : %d\n",ref[i]);
+            printf("\tHIT: %d\n",ref[i]);
         }
         else{
-            printf("\tPage fault : %d\n",ref[i]);
+            printf("\tFAULT: %d\n",ref[i]);
         }
+        printf("\n");
     }
-
-    //printing results
-    printf("\nPage hit: %d\n",pageHit);
-    printf("\nPage fault: %d\n",pageFault);
-    return(0);
-    
+    printf("Total page hit : %d\n",pageHit);
+    printf("Total page fault : %d\n",pageFault);
+    return EXIT_SUCCESS;
 }
+
